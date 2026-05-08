d77f14b5b85cab0afa2c3335bf5cd711aeb3fd21
rafaeelb <124620790+rafaeelb@users.noreply.github.com>
2026-05-08 19:45:42 -0300

    Update algoritmo34.py

--- Redes neurais/algoritmo34.py
+++ Redes neurais/algoritmo34.py
@@ -70,7 +70,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 model = MLPClassifier(
     hidden_layer_sizes=(32,16),
     activation='relu',
-    max_iter=100
+    max_iter=1000
 )
 
 model.fit(X_train, y_train)
